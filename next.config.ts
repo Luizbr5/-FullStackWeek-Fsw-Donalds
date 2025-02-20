d806f487e93a0f803af572ef352a49d675f366f9
Luizbr5 <luixbr1@gmail.com>
2025-02-20 12:01:15 -0300

    feat add slug and src

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["u9a6wmr3as.ufs.sh"], // Add your domain here
+  },
+};
+
+module.exports = {
+  images: {
+    domains: ["u9a6wmr3as.ufs.sh"], // Adicione seu domínio aqui
+  },
 };
 
 export default nextConfig;
