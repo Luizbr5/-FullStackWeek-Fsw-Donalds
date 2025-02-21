9a559c6b0a73ce874a56a96ac45beb5fcddd42d0
Luizbr5 <luixbr1@gmail.com>
2025-02-21 00:10:55 -0300

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/helpers/format-currency";
+
 interface productsProps{
     products: Product[];
 }
@@ -20,7 +22,7 @@ const Products = ({ products }: productsProps) => {
                         {product.description}
                     </p>
                     <p className="pt-3 text-sm font-semibold">
-                        {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price)}</p>
+                        {formatCurrency(product.price)}</p>
                 </div>
 
                 {/* DIREITA */}
