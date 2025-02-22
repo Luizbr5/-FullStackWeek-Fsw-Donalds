a7f890f7cd7f07aacead0d558e713d67a515765f
Luizbr5 <luixbr1@gmail.com>
2025-02-22 10:16:00 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -54,4 +55,5 @@ export const createOrder = async (input: CreateOrderInput) => {
            restaurantId: restaurant.id
         },
     });
+    redirect (`/${input.slug}/orders`);
 };
